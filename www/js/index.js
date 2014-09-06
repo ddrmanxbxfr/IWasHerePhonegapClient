18105d0f50bb9e7c8728d56d31879ec0d85a6176
Mathieu Rhéaume <mathieu@codingrhemes.com>
2014-09-06 19:20:44 -0400

    refactored methods

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,38 @@
-window.onload = function () {
+/*global console */
+
+function demarerCamera() {
+    "use strict";
+    // Grab elements, create settings, etc.
+    var canvas = document.getElementById("canvas"),
+        context = canvas.getContext("2d"),
+        video = document.getElementById("video"),
+        videoObj = {
+            "video": true
+        },
+        errBack = function (error) {
+            console.log("Video capture error: ", error.code);
+        };
+
+    // Put video listeners into place
+    if (navigator.getUserMedia) { // Standard
+        navigator.getUserMedia(videoObj, function (stream) {
+            video.src = stream;
+            video.play();
+        }, errBack);
+    } else if (navigator.webkitGetUserMedia) { // WebKit-prefixed
+        navigator.webkitGetUserMedia(videoObj, function (stream) {
+            video.src = window.webkitURL.createObjectURL(stream);
+            video.play();
+        }, errBack);
+    } else if (navigator.mozGetUserMedia) { // Firefox-prefixed
+        navigator.mozGetUserMedia(videoObj, function (stream) {
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+        }, errBack);
+    }
+}
+
+function configurerBtnEvents() {
     "use strict";
     var slideMenuButton = document.getElementById('slide-menu-button');
     slideMenuButton.onclick = function (e) {
@@ -9,4 +43,10 @@ window.onload = function () {
             cl.add('left-nav');
         }
     };
+}
+
+window.onload = function () {
+    "use strict";
+    configurerBtnEvents();
+    demarerCamera();
 };
