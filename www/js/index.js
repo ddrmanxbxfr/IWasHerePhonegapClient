6fdda36591616507ec0133be21babe18af4ca5ea
Mathieu Rhéaume <mathieu@codingrhemes.com>
2014-09-08 00:33:26 -0400

    refactored stuff

--- www/js/index.js
+++ www/js/index.js
@@ -1,114 +1,10 @@
 /*global console, onSuccessGeoLoc, onErrorGeoLoc, watchGeoID, isUserGeoLocated, showOverlay, startGeolocating,
 stopGeolocating, currentGeoCoords,$, hideOverlay, ich*/
 
-
-function removeClass(documentToWorkOn, className) {
+function setupSideMenuBtn() {
     "use strict";
-    if (documentToWorkOn.classList.contains(className)) {
-        documentToWorkOn.classList.remove(className);
-    }
-}
-
-function addClass(documentToWorkOn, className) {
-    "use strict";
-    if (documentToWorkOn.classList.contains(className) === false) {
-        documentToWorkOn.classList.add(className);
-    }
-}
-
-function overlay_sendToAPI_HideOverlay() {
-    "use strict";
-    var btnSendToApiClose;
-    btnSendToApiClose = document.getElementById('btnSendToApiClose');
-
-    addClass(btnSendToApiClose, 'off');
-
-    startGeolocating();
-
-    hideOverlay('overlay_sendingToAPI');
-}
-
-function overlay_sendToAPI_LoadingDone() {
-    "use strict";
-    var elementToChange, btnSendToApiClose;
-    elementToChange = document.getElementById('currentProgress');
-    btnSendToApiClose = document.getElementById('btnSendToApiClose');
-    // List of class to swap fa-circle-o-notch fa-spin
-    removeClass(elementToChange, 'fa-spin');
-    removeClass(elementToChange, 'fa-circle-o-notch');
-    removeClass(btnSendToApiClose, 'off');
-    addClass(elementToChange, 'fa-check-square');
-    addClass(elementToChange, 'green-icon');
-
-    document.getElementById('sendToApiTxtProgress').textContent = "Thanks for marking your territory here !";
-}
-
-function overlay_sendToAPI_SetUIForLoading() {
-    "use strict";
-    var elementToChange, btnSendToApiClose;
-    elementToChange = document.getElementById('currentProgress');
-    btnSendToApiClose = document.getElementById('btnSendToApiClose');
-    document.getElementById('sendToApiTxtProgress').textContent = "Please wait while our dog chews your infos.";
-    addClass(elementToChange, 'fa-spin');
-    addClass(elementToChange, 'fa-circle-o-notch');
-    addClass(btnSendToApiClose, 'off');
-    removeClass(elementToChange, 'fa-check-square');
-    removeClass(elementToChange, 'green-icon');
-}
-
-function sendToApi() {
-    "use strict";
-
-    function createGeoJsonFromProps(txtIfPresent) {
-        var baseData = {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [currentGeoCoords.lat, currentGeoCoords.lng]
-            },
-            "properties": {
-                geoAccuracy: currentGeoCoords.accuracy
-            }
-        };
-
-        if (txtIfPresent !== undefined && txtIfPresent.length > 0) {
-            baseData.properties.textNote = txtIfPresent;
-        }
-        return baseData;
-    }
-    var txtArea, request, data;
-    txtArea = document.getElementById('txtNote').textContent;
-
-    data = createGeoJsonFromProps(txtArea);
-
-    // Do prepare the request to send to api...
-    $.post('http://192.168.2.26:4711/api/iwashere', data, function (response) {
-        if (document.getElementById('overlay_sendingToAPI').classList.contains('off') === false) {
-            overlay_sendToAPI_LoadingDone();
-        }
-    });
-
-    hideOverlay('overlay_leaveAMessage');
-    showOverlay('overlay_sendingToAPI');
-    overlay_sendToAPI_SetUIForLoading();
-}
-
-
-function modalMarkTerritory() {
-    "use strict";
-    if (isUserGeoLocated && currentGeoCoords !== undefined) {
-        stopGeolocating();
-        showOverlay('overlay_leaveAMessage');
-    }
-}
-
-function configurerBtnEvents() {
-    "use strict";
-    var slideMenuButton, btnMarkYourTerritory, btnSendToApi, btnSendToApiClose;
+    var slideMenuButton;
     slideMenuButton = document.getElementById('slide-menu-button');
-    btnMarkYourTerritory = document.getElementById('roundBtn');
-    btnSendToApi = document.getElementById('btnSendToApi');
-    btnSendToApiClose = document.getElementById('btnSendToApiClose');
     slideMenuButton.onclick = function (e) {
         if (document.body.classList.contains('left-nav')) {
             document.body.classList.remove('left-nav');
@@ -116,33 +12,7 @@ function configurerBtnEvents() {
             document.body.classList.add('left-nav');
         }
     };
-
-    btnMarkYourTerritory.onclick = modalMarkTerritory;
-    btnSendToApi.onclick = sendToApi;
-    btnSendToApiClose.onclick = overlay_sendToAPI_HideOverlay;
 }
-
-function updateUiGeoConfirmation(isGeoReady) {
-    "use strict";
-    var docToUpdate, btnMark;
-    docToUpdate = document.getElementById('geolocationStatus');
-    btnMark = document.getElementById('roundBtn');
-    if (isGeoReady) {
-        docToUpdate.textContent = "Ok ! you're ready to mark your territory";
-
-        removeClass(btnMark, 'button-not-ready');
-        removeClass(btnMark, 'round-button-circle-not-ready');
-        addClass(btnMark, 'round-button-circle-ready');
-        addClass(btnMark, 'button-ready');
-    } else {
-        docToUpdate.textContent = "We're preparing your black sharpie to write down a message here. Hold on.";
-        addClass(btnMark, 'button-not-ready');
-        addClass(btnMark, 'round-button-circle-not-ready');
-        removeClass(btnMark, 'round-button-circle-ready');
-        removeClass(btnMark, 'button-ready');
-    }
-}
-
 
 /* Stuff happends when app starts... */
 document.addEventListener("deviceready", startGeolocating, false);
@@ -151,6 +21,7 @@ window.onload = function () {
     "use strict";
     var template = ich.TemplateMarkTerritory();
     $('#main-content').append(template);
-    configurerBtnEvents();
+    setupSideMenuBtn();
+    setupBtnMarkTerritory();
     updateUiGeoConfirmation(false);
 };
